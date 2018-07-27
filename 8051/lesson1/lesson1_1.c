6c66038f7ac43803197eb6f56a7efbfa947a7473
lishijm <lishijm@outlook.com>
2018-07-27 10:29:07 +0800

    8051

---
+++ 8051/lesson1/lesson1_1.c
@@ -0,0 +1,11 @@
+#include<reg52.h>
+sbit D1=P1^0; 
+int main()
+{
+    D1=0;
+    D1=1;
+    D1=2;
+    D1=3;
+    D1=4;
+	D1=0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Learning-C
 #包含部分《C Primer Plus》和《The C Programming Language》的习题
 #包含部分XUPT和HDU的OJ习题
+#包含部分8051单片机的工程
