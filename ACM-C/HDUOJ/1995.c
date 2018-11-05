2ccdaed69ce15de7575776e74b53aec2afc8d791
lishijm <lishijm@outlook.com>
2018-11-05 22:15:40 +0800

    change

--- ACM-C/HDUOJ/1995.c
+++ ACM-C/HDUOJ/1995.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-void hanoi(int n,int k){
-    int num 
+int hanoi(int n,int k){
+    int num;
+    
 }
 int main(int argc, char const *argv[])
 {
@@ -10,7 +11,7 @@ int main(int argc, char const *argv[])
     scanf("%d",&t);
     for(i=0;i<t;i++){
         scanf("%d %d",&h[i][0],&h[i][1]);
-        result=hanoi(h[i][0,h[i][1]);
+        result=hanoi(h[i][0],h[i][1]);
         printf("%d",result);
     }
     return 0;
