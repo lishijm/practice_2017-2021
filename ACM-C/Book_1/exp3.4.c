9f26273d75ee2dbe7f5957b869ecfb81a2969a45
lishijm <lishijm@outlook.com>
2018-11-24 21:12:13 +0800

    add

--- ACM-C/Book_1/exp3.4.c
+++ ACM-C/Book_1/exp3.4.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 int main(int argc, char const *argv[])
 {
-    int 
+    int count=0;
+    char s[20],buf[99];
+    scanf("%s",&s);
+    for(int abc=111;abc<=999;abc++){
+        for(int de=11;de<=99;de++){
+            int x=abc*(de%10),y=abc*(de/10),z=abc*de;
+            sprintf(buf,"%d%d%d%d",abc,de,x,y,z);
+            int ok=1;
+            for(int i=0;i<strlen(buf);i++){
+                if(strchr(s,buf[i])==NULL){
+                    ok=0;
+                }
+            }
+            if(ok){
+                
+            }
+        }
+    }
     return 0;
 }
