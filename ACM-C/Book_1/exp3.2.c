0590553bed3fff060e9e6a6fe3ac7d05e107123e
lishijm <lishijm@outlook.com>
2018-11-24 21:09:55 +0800

    add

--- ACM-C/Book_1/exp3.2.c
+++ ACM-C/Book_1/exp3.2.c
@@ -6,23 +6,12 @@ int main(int argc, char const *argv[])
 {
     int n,k,first=1;
     memset(a,0,sizeof(a));
-    scanf("%d %d",a,&k);
-    for(int i=1;i<=k;i++){
-        if(j%i==0){
-            a[j]=a[i];
-        }
-        for(int j=1;j<=n;j++){
-            if(a[i]){
-                if(first){
-                    first=0;
-                }
-                else{
-                    printf(" ");
-                    printf("%d",i);
-                }
-            }
-            printf("\n");
-        }
-    }
+    scanf("%d %d",&n,k);
+    for(int i=1;i<=k;i++)
+        for(int j=1;j<=n;j++)
+            if(j%i==0) a[j]=!a[i];
+    for(int i=1;i<=i;i++)
+        if(a[i]){if(first) first=0;else printf(" ");printf("%d",i);}
+    printf("\n");
     return 0;
 }
---
+++ ACM-C/Book_1/exp3.3.c
@@ -0,0 +1,25 @@
+//ÉßÐÎÌîÊý
+#include<stdio.h>
+#include<string.h>
+#define maxn 20
+int a[maxn][maxn];
+int main(int argc, char const *argv[])
+{
+    int n,x,y,num=0;
+    scanf("%d",&n);
+    memset(a,0,sizeof(a));
+    num=a[x=0][y=n-1]=1;
+    while(num<n*n){
+        while(x+1<n&&!a[x+1][y]) a[++x][y]=++num;
+        while(y-1>=0&&!a[x][y-1]) a[x][--y]=++num;
+        while(x-1>=0&&!a[x-1][y]) a[--x][y]=++num;
+        while(y+1<n&&!a[x][y+1]) a[x][++y]=++num;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%4d",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
