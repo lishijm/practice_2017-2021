da2a65860b0707b90b49fbb9fa52ae4d46d14fab
lishijm <lishijm@outlook.com>
2018-12-08 15:29:52 +0800

    change

--- Arduino/Counter/counter_v0.1/counter_v0.1.ino
+++ Arduino/Counter/counter_v0.1/counter_v0.1.ino
@@ -6,7 +6,7 @@ LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
 //以下字库由二进制转为十六进制
 uint8_t ren[8]={0x02,0x02,0x04,0x04,0x04,0x0A,0x0A,0x11,};//“人”字库
 uint8_t ci[8]={0X04,0X17,0X15,0X02,0X0A,0X15,0X15,0X15,};//“次”字库
-int nop=0;
+int nop=0;               //人次的变量
 void setup(){
   pinMode(2,INPUT); 
   lcd.begin(16,2);      //红外开关数据端口
