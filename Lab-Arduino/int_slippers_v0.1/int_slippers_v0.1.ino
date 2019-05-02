bdd3606d3ba57b3850aa697f0cb23ed18ee2245d
lishijm <lishijm@outlook.com>
2019-05-02 20:58:48 +0800

    加热第一版本

--- Lab-Arduino/int_slippers_v0.1/int_slippers_v0.1.ino
+++ Lab-Arduino/int_slippers_v0.1/int_slippers_v0.1.ino
@@ -1,9 +1,36 @@
+#include<Arduino.h>
+#include<OneWire.h>
+#include<DallasTemperature.h>
+#define BUS 2
+OneWire onewire(BUS);
+DallasTemperature sensors(&onewire);
 void setup() {
-  // put your setup code here, to run once:
-
+  //6.8k (DS18b20数字输出引脚需要接4.7k-10k上拉电阻)
+  //针脚定义 面朝印字面，左为GND,右为VCC,中间为数字输出引脚（接4.7-10k上拉电阻）
+  Serial.begin(9600);
+  sensors.begin();
 }
-
+ 
 void loop() {
   // put your main code here, to run repeatedly:
-
+   sensors.requestTemperatures();
+   Serial.print(sensors.getTempCByIndex(0));
+   Serial.print("℃\n");
+   delay(1000);
 }
+
+/*const int relayPin =7; //the "s" of relay module attach to
+
+void setup()
+{
+  pinMode(relayPin, OUTPUT); //initialize relay as an output
+}
+
+void loop()
+{
+  digitalWrite(relayPin, HIGH); //Close the relay
+  delay(1000); //wait for 1 second
+  digitalWrite(relayPin, LOW); //disconnect the relay 
+  delay(1000); //wait for 1 second
+}
+*/
