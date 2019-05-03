70f544dc3678edf5a8820f2d4d92da1ffa063130
lishijm <lishijm@outlook.com>
2019-05-03 21:59:33 +0800

    0.1(over)

--- Lab-Arduino/int_slippers_v0.1/int_slippers_v0.1.ino
+++ Lab-Arduino/int_slippers_v0.1/int_slippers_v0.1.ino
@@ -1,37 +1,44 @@
-//只实现恒温加热
+//只实现恒温加热功能
 #include<Arduino.h>
 #include<OneWire.h>
 #include<DallasTemperature.h>
-#define BUS 2
-OneWire onewire(BUS);
+//#include<LiquidCrystal.h>
+
+#define sensora 2
+#define sensorb 3
+const int repin=8;
+int tmpc;  //设定的温度变量
+int tmp1;  //一号传感器的温度
+//int tmp2;  //二号传感器的温度
+int flag=0; //判断是非设定温度
+OneWire onewire(sensora);
 DallasTemperature sensors(&onewire);
-void setup() {
-  //6.8k (DS18b20数字输出引脚需要接4.7k-10k上拉电阻)
-  //针脚定义 面朝印字面，左为GND,右为VCC,中间为数字输出引脚（接4.7-10k上拉电阻）
-  Serial.begin(9600);
+
+void setup(){
   sensors.begin();
+  pinMode(repin,OUTPUT);
+  Serial.begin(9600); //串口监视波率
 }
- 
-void loop() {
-  // put your main code here, to run repeatedly:
-   sensors.requestTemperatures();
-   Serial.print(sensors.getTempCByIndex(0));
-   Serial.print("℃\n");
-   delay(1000);
-}
+void loop(){
+  //串口数据读取
+  if(flag==0){
+    tmpc=Serial.parseInt();
+  }
+  if(tmpc>0){
+    flag=1;
+  }
+  
+  sensors.requestTemperatures();
+  tmp1=sensors.getTempCByIndex(0);
+  Serial.print(tmp1);
+  Serial.print("℃\n");
+  //tmpc后的数值为消抖所设定的温度阈值，其准确值待明日测试时考证
+  if(tmp1<=tmpc-2){
+    digitalWrite(repin,HIGH);
+  }
+  if(tmp1>tmpc+1){
+    digitalWrite(repin,LOW);
+  }
 
-/*const int relayPin =7; //the "s" of relay module attach to
-
-void setup()
-{
-  pinMode(relayPin, OUTPUT); //initialize relay as an output
+  delay(1000);
 }
-
-void loop()
-{
-  digitalWrite(relayPin, HIGH); //Close the relay
-  delay(1000); //wait for 1 second
-  digitalWrite(relayPin, LOW); //disconnect the relay 
-  delay(1000); //wait for 1 second
-}
-*/
