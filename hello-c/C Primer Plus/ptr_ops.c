dc3ea1a7e80d0b84effad7964f1c3d2cfd40e608
lishijm <lishijm@outlook.com>
2023-02-07 11:32:56 +0800

    point

---
+++ hello-c/C Primer Plus/ptr_ops.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int urn[5]={100,200,300,400,500};
+    int *ptr1,*ptr2,*ptr3;
+    ptr1=urn;
+    ptr2=&urn[2];
+
+    printf("pointer value,dereferenced pointer,pointer address:\n");
+    printf("ptr1 =%p,*ptr1=%d,&ptre1=%p\n",ptr1,*ptr1,&ptr1);
+
+    return 0;
+}
